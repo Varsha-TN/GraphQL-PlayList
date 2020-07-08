5dfc4180cbfb82f7117b7e30b3d36bfe2e077958
TN <varsha.tn@dxc.com>
2020-07-08 13:31:53 +0530

    First commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
+
+// connect to mlab database
+mongoose.connect('mongodb://localhost:27017/mylib' , { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
+
+// binding express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
